11d373f08e0f9bacc7b7dbe02f4302f055eb3e47
Dhiman <dhimannayak003@gmail.com>
2024-10-04 15:41:12 +0530

    minor change

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app=express();
 app.use(cors({
-    origin: 'http://localhost:4000',
+    origin: 'http://localhost:3000',
     credentials:true
 }));
 
